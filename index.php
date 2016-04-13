7cff34405a5584fd21881319acd6e75022cab4a5
Ezi0_@udit0re <amannagpal4@gmail.com>
2016-04-13 13:55:31 +0530

    media queries for different screen sizes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Title</title>
+    <title>Flobox</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
@@ -19,42 +19,10 @@
     <link rel="stylesheet" href="./index.css">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
     <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Delicious">
 </head>
 <body>
-  
-    <!-- Navbar !-->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>                        
-                </button>
-                 
-                <a class="navbar-brand" href="#">FloBox</a>
-            </div>
-            <?php
-                if(!$detect->isMobile()) 
-                {
-            ?>
-            <h5 id="banner" class="navbar-text">JOIN TODAY TO WIN A YEAR’S FREE SUBSCRIPTION ! </h5>
-            <?php } ?>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#about">About us</a></li>
-                    <li><a href="#contact">Contact us</a></li>
-                </ul>
-            </div>
-        </div>    
-    </nav>
-    <?php if($detect->isMobile()) { ?>
-        <div class="navbar navbar-fixed-bottom navbar-inverse footer" >
-                <h5 style="color: #5bc0de">JOIN TODAY TO WIN A YEAR’S FREE SUBSCRIPTION ! </h5>
-        </div> 
-    <?php }?>    
-  <!-- Background image div !-->
-    <div id="first-image">
+  <div id="first-image">
         <div id="name">
             <span id="bold">FloBox:</span><br>
             <?php
@@ -89,6 +57,39 @@
         </div>
     </div>    
 
+    <!-- Navbar !-->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+                 
+                <a class="navbar-brand" href="#">FloBox</a>
+            </div>
+            <?php
+                if(!$detect->isMobile()) 
+                {
+            ?>
+            <h5 id="banner" class="navbar-text">JOIN TODAY TO WIN A YEAR’S FREE SUBSCRIPTION ! </h5>
+            <?php } ?>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#about">About us</a></li>
+                    <li><a href="#contact">Contact us</a></li>
+                </ul>
+            </div>
+        </div>    
+    </nav>
+    <?php if($detect->isMobile()) { ?>
+        <div class="navbar navbar-fixed-bottom navbar-inverse footer" >
+                <h5 style="color: #5bc0de">JOIN TODAY TO WIN A YEAR’S FREE SUBSCRIPTION ! </h5>
+        </div> 
+    <?php }?>    
+  <!-- Background image div !-->
+    
   
     
   
@@ -102,6 +103,7 @@
         <div class="col-md-5 <?php if(!$detect->isMobile()) echo "pull-right"; ?>" id="contact">
             <h2>Contact us</h2>
             <p>Drop us an email at</p>
+            
         </div>
    </div>     
 
